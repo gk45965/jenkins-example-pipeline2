a663b353a494b5b626d02e60446d867f678814d1
83Incs_11 <gaurav.kumar@83incs.com>
2019-12-31 07:42:09 +0530

    first commit

---
+++ src/main/java/com/example/jenkinsexamplepipeline2/JenkinsExamplePipeline2Application.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsexamplepipeline2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsExamplePipeline2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsExamplePipeline2Application.class, args);
+	}
+
+}
